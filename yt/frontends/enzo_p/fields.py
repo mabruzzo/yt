3ba2984192df1129dfb73ae3cd79f12ccd88d2d9
mabruzzo <matthewabruzzo@gmail.com>
2019-10-07 13:38:35 -0400

    Added temperature and metal_density fields. Also refactored how nodal fields are set up to be more general.

--- yt/frontends/enzo_p/fields.py
+++ yt/frontends/enzo_p/fields.py
@@ -18,7 +18,7 @@ from yt.fields.field_info_container import \
 from yt.fields.particle_fields import \
     add_union_field
 from yt.frontends.enzo_p.misc import \
-    nested_dict_get
+    nested_dict_get, field_subgroup_generator
 
 rho_units = "code_mass / code_length**3"
 vel_units = "code_velocity"
@@ -53,6 +53,8 @@ class EnzoPFieldInfo(FieldInfoContainer):
         ("bfieldi_x", (b_units, [], None)),
         ("bfieldi_y", (b_units, [], None)),
         ("bfieldi_z", (b_units, [], None)),
+        ("metal_density", (rho_units, ["metal_density"], None)),
+        ("temperature", ("K", ["temperature"], None)),
     )
 
     known_particle_fields = (
@@ -72,11 +74,38 @@ class EnzoPFieldInfo(FieldInfoContainer):
         super(EnzoPFieldInfo, self).__init__(
             ds, field_list, slice_info=slice_info)
 
-        # setup nodal flag information
-        for field in NODAL_FLAGS:
-            if ('enzop', field) in self:
-                finfo = self['enzop', field]
-                finfo.nodal_flag = np.array(NODAL_FLAGS[field])
+        self.setup_nodal_flags(ds)
+
+    def setup_nodal_flags(self,ds):
+        # set's up all nodal flags based on the field centering specified in
+        # the parameter file
+
+        # retrieve list of known fields
+        known_fields = frozenset((elem[0] for elem in self.known_other_fields))
+
+        # centering is specified in the "Fields" section of the parameter file
+        # in subgroups named after defined fields
+        for field_name, subgroup in field_subgroup_generator(ds.parameters):
+            # retrieve expected nodal flags (if applicable)
+            expected_flags = None
+            if field_name in known_fields:
+                expected_flags = np.array(NODAL_FLAGS.get(field_name,[0,0,0]))
+
+            specified = subgroup.get('centering')
+            if specified is not None:
+                nodal_flags = np.logical_not(np.array(specified)).astype(int)
+                if ((expected_flags is not None) and
+                    (expected_flags != nodal_flags).any()):
+                    msg = 'expected "{}" to have {!s} centering, not {!s}.'
+                    raise ValueError(msg.format(field_name,expected_flags,
+                                                nodal_flags))
+                finfo = self['enzop', field_name]
+                finfo.nodal_flag = np.array(nodal_flags)
+
+            elif ((expected_flags is not None)
+                  and (expected_flags != np.array([0,0,0])).any()):
+                msg = 'expected "{}" to have {} centering, not [0,0,0].'
+                raise ValueError(msg.format(field_name,str(expected_flags)))
 
     def setup_fluid_fields(self):
         from yt.fields.magnetic_field import \
@@ -87,34 +116,42 @@ class EnzoPFieldInfo(FieldInfoContainer):
 
     def setup_energy_field(self):
         unit_system = self.ds.unit_system
-        # check if we need to include magnetic energy
-        has_magnetic = ('bfield_x' in self.ds.parameters['Field']['list'])
 
-        if not has_magnetic:
-            # thermal energy = total energy - kinetic energy
-            def _tot_minus_kin(field, data):
-                ret = data["total_energy"] - 0.5*data["velocity_x"]**2.0
-                if data.ds.dimensionality > 1:
-                    ret -= 0.5*data["velocity_y"]**2.0
-                if data.ds.dimensionality > 2:
-                    ret -= 0.5*data["velocity_z"]**2.0
-                return ret
-            self.add_field(("gas", "thermal_energy"), sampling_type="cell",
-                           function = _tot_minus_kin,
-                           units = unit_system["specific_energy"])
+        ppm_param = self.ds.parameters.get("ppm",None)
+        dual_energy =  ((ppm_param is not None)
+                        and ppm_param.get("dual_energy",False))
+        if dual_energy:
+            self.alias(("gas", "thermal_energy"), ("enzop", "internal_energy"))
+
         else:
-            # thermal energy = total energy - kinetic energy - magnetic energy
-            def _sub_b(field, data):
-                ret = data["total_energy"] - 0.5*data["velocity_x"]**2.0
-                if data.ds.dimensionality > 1:
-                    ret -= 0.5*data["velocity_y"]**2.0
-                if data.ds.dimensionality > 2:
-                    ret -= 0.5*data["velocity_z"]**2.0
-                ret -= data["magnetic_energy"]/data["density"]
-                return ret
-            self.add_field(("gas", "thermal_energy"), sampling_type="cell",
-                           function=_sub_b,
-                           units = unit_system["specific_energy"])
+            # check if we need to include magnetic energy
+            has_magnetic = ('bfield_x' in self.ds.parameters['Field']['list'])
+
+            if not has_magnetic:
+                # thermal energy = total energy - kinetic energy
+                def _tot_minus_kin(field, data):
+                    ret = data["total_energy"] - 0.5*data["velocity_x"]**2.0
+                    if data.ds.dimensionality > 1:
+                        ret -= 0.5*data["velocity_y"]**2.0
+                    if data.ds.dimensionality > 2:
+                        ret -= 0.5*data["velocity_z"]**2.0
+                    return ret
+                self.add_field(("gas", "thermal_energy"), sampling_type="cell",
+                               function = _tot_minus_kin,
+                               units = unit_system["specific_energy"])
+            else:
+                # thermal = total energy - kinetic energy - magnetic energy
+                def _sub_b(field, data):
+                    ret = data["total_energy"] - 0.5*data["velocity_x"]**2.0
+                    if data.ds.dimensionality > 1:
+                        ret -= 0.5*data["velocity_y"]**2.0
+                    if data.ds.dimensionality > 2:
+                        ret -= 0.5*data["velocity_z"]**2.0
+                    ret -= data["magnetic_energy"]/data["density"]
+                    return ret
+                self.add_field(("gas", "thermal_energy"), sampling_type="cell",
+                               function=_sub_b,
+                               units = unit_system["specific_energy"])
         
 
     def setup_particle_fields(self, ptype, ftype='gas', num_neighbors=64):
