fe3bae44531ef1b5121598d837103cb4fac7916d
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-11-04 10:15:55 -0500

    further rework test_cholla_load.py

--- yt/frontends/cholla/tests/test_cholla_load.py
+++ yt/frontends/cholla/tests/test_cholla_load.py
@@ -15,9 +15,8 @@ In more detail, Cholla has had a couple of historical data formats.
     between the 2 packages
 """
 
+import dataclasses
 import typing
-from collections.abc import Sequence
-from dataclasses import dataclass
 
 import numpy as np
 import pytest
@@ -34,12 +33,88 @@ else:
     from yt.utilities.on_demand_imports import _h5py as h5py
 
 
-@dataclass(frozen=True)
+@dataclasses.dataclass(frozen=True, kw_only=True)
 class DomainInfo:
     """Characterizes the domain of the synthetic simulation"""
 
     domain_left_edge_kpc: tuple[float, float, float]
     domain_width_kpc: tuple[float, float, float]
+
+    def __post_init__(self):
+        if len(self.domain_left_edge_kpc) != 3:
+            raise ValueError("domain_left_edge_kpc must be a 3 element tuple")
+        elif len(self.domain_width_kpc) != 3:
+            raise ValueError("domain_width_kpc must be a 3 element tuple")
+        elif any(e <= 0 for e in self.domain_width_kpc):
+            raise ValueError("domain_width_kpc must contain positive values")
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class SyntheticDatasetInfo:
+    """Tracks properties of a synthetic Cholla dataset"""
+
+    # Specifies the number of processes that the hypothetical Cholla simulation used
+    nprocs: tuple[int, int, int]
+    # Specifies the global shape of each field in the hypothetical Cholla simulation
+    # (each MPI process would be responsible for evolving a subsection of the global
+    # shape).
+    global_shape: tuple[int, int, int]
+    # Specifies domain information about the synthetic simulation
+    domain_info: DomainInfo
+    # the fields in the synthetic dataset. Aside: particle datafiles must record the
+    # number of fields
+    field_names: list[str]
+
+    def __post_init__(self):
+        # a lot of these checks were inherited from when these attributes
+        # were direct function arguments
+
+        if any(not isinstance(name, str) for name in self.field_names):
+            raise TypeError("field_names must be a sequence of strings")
+        elif isinstance(self.field_names, str):
+            raise TypeError("field_names can't be a string")
+        elif len(self.field_names) != len(set(self.field_names)):
+            raise ValueError("field_names must hold unique names")
+
+        if len(self.nprocs) != 3:
+            raise ValueError("nprocs must be a 3 element array")
+        elif any(int(e) != e for e in self.nprocs):
+            raise ValueError("nprocs must contain integers")
+        elif any(e < 1 for e in self.nprocs):
+            raise ValueError("nprocs must contain positive values")
+        else:
+            object.__setattr__(self, "nprocs", tuple(int(e) for e in self.nprocs))
+
+        if len(self.global_shape) != 3:
+            raise ValueError("global_shape must be a 3 element array")
+        elif any(int(e) != e for e in self.global_shape):
+            raise ValueError("global_shape must contain integers")
+        elif any(e < 1 for e in self.global_shape):
+            raise ValueError("global_shape must contain positive values")
+        else:
+            object.__setattr__(
+                self, "global_shape", tuple(int(e) for e in self.global_shape)
+            )
+
+        # infer the shape of each block (and perform a sanity check)
+        cc_block_shape, remainder = np.divmod(self.global_shape, self.nprocs)
+        if (cc_block_shape == 0).any():
+            raise ValueError(
+                "nprocs contains a value exceeding the corrsponding length in "
+                "global_shape"
+            )
+        elif (remainder != 0).any():
+            raise ValueError(
+                "at least 1 element of global_shape isn't evenly divisible by nprocs"
+            )
+
+    @property
+    def cc_block_shape(self) -> tuple[int, int, int]:
+        """The cell-centered block shape"""
+        return tuple(np.floor_divide(self.global_shape, self.nprocs))
+
+    def get_blockid_location_array(self) -> np.ndarray:
+        return np.arange(np.prod(self.nprocs)).reshape(self.nprocs)
 
 
 def _generate_array(shape: tuple[int, ...], *, start: int = 0):
@@ -90,12 +165,8 @@ def _write_domain_prop_attrs(
 
 def _generate_files(
     root_path: str,
-    nprocs: Sequence[int],
-    global_shape: Sequence[int],
-    domain_info: DomainInfo,
-    *,
-    data_format: ChollaDataFmt | None = None,
-    field_names: Sequence[str] | None = None,
+    dset_info: SyntheticDatasetInfo,
+    data_format: ChollaDataFmt,
 ) -> tuple[dict[str, np.ndarray], str]:
     """
     Generates file(s) that emulate a dataset holding results from a
@@ -105,17 +176,10 @@ def _generate_files(
     ----------
     root_path
         Prefix of the path where the dataset is written
-    nprocs
-        Specifies the number of processes that the hypothetical Cholla
-        simulation used
-    global_shape
-        Specifies the global shape of each field in the hypothetical
-        Cholla simulation (each MPI process would be responsible for evolving
-        a subsection of the global shape).
+    dset_info
+        Specifies properties number of the synthetic dataset
     data_format
         Specifies the format of the dataset
-    field_names
-        The names of the fields to include in the output dataset
 
     Returns
     -------
@@ -125,71 +189,34 @@ def _generate_files(
         Path to one of the files. For distributed datasets this is
         always process 0.
     """
-    # check and sanitize arguments
-
-    if isinstance(field_names, str):
-        raise TypeError("field_names can't be a string")
-    elif field_names is None:
-        field_names = ["density"]
-    elif len(field_names) != len(set(field_names)):
-        raise ValueError("field_names must hold unique names")
-
-    if len(nprocs) != 3:
-        raise ValueError("nprocs must be a 3 element array")
-    elif any(int(e) != e for e in nprocs):
-        raise ValueError("nprocs must contain integers")
-    elif any(e < 1 for e in nprocs):
-        raise ValueError("nprocs must contain positive values")
-    else:
-        nprocs = tuple(int(e) for e in nprocs)
-
-    if len(global_shape) != 3:
-        raise ValueError("global_shape must be a 3 element array")
-    elif any(int(e) != e for e in global_shape):
-        raise ValueError("global_shape must contain integers")
-    elif any(e < 1 for e in global_shape):
-        raise ValueError("global_shape must contain positive values")
-    else:
-        global_shape = tuple(int(e) for e in global_shape)
-
-    # infer the shape of each block (and perform a sanity check)
-    cc_block_shape, remainder = np.divmod(global_shape, nprocs)
-    if (cc_block_shape == 0).any():
-        raise ValueError(
-            "nprocs contains a value exceeding the corrsponding length in global_shape"
-        )
-    elif (remainder != 0).any():
-        raise ValueError(
-            "at least 1 element of global_shape isn't evenly divisible by nprocs"
-        )
-
     if data_format is ChollaDataFmt.LEGACY_CONCAT:
-        cc_block_shape = np.array(global_shape)
-        nprocs = (1, 1, 1)
+        dset_info = dataclasses.replace(dset_info, nprocs=(1, 1, 1))
+
+    cc_block_shape = dset_info.cc_block_shape
 
     # construct the global arrays
     global_arrays = {}
     start_offset = 0
-    for name in field_names:
-        arr = _generate_array(global_shape, start=start_offset)
+    for name in dset_info.field_names:
+        arr = _generate_array(dset_info.global_shape, start=start_offset)
         global_arrays[name] = arr
         start_offset = arr.max() + 1
 
     # prepare to creating the files
-    blockid_location_arr = np.arange(np.prod(nprocs)).reshape(nprocs)
+    blockid_location_arr = dset_info.get_blockid_location_array()
     match data_format:
         case ChollaDataFmt.DISTRIBUTED:
             fname_template = f"{root_path}.h5.{{blockid:d}}"
             field_grp = "/"
-            field_dset_shape = tuple(cc_block_shape)
+            field_dset_shape = cc_block_shape
         case ChollaDataFmt.LEGACY_CONCAT:
             fname_template = f"{root_path}.h5"
             field_grp = "/"
-            field_dset_shape = global_shape
+            field_dset_shape = cc_block_shape
         case ChollaDataFmt.CONCAT:
             fname_template = f"{root_path}.h5"
             field_grp = "field"
-            field_dset_shape = (np.prod(nprocs),) + tuple(cc_block_shape)
+            field_dset_shape = (np.prod(dset_info.nprocs),) + cc_block_shape
         case _:
             raise RuntimeError(f"unknown data format: {data_format}")
 
@@ -212,7 +239,7 @@ def _generate_files(
                     dst_idx = (...,)
 
                     f.attrs["offset"] = np.array([int(slc.start) for slc in src_slc])
-                    f.attrs["dims_local"] = cc_block_shape
+                    f.attrs["dims_local"] = np.array(cc_block_shape)
 
                 case ChollaDataFmt.LEGACY_CONCAT:
                     dst_idx = (...,)
@@ -234,70 +261,78 @@ def _generate_files(
 
             if (blockid == 0) or (not data_format.is_single_file):
                 # write some common metadata
-                f.attrs["dims"] = np.array(global_shape)
-                f.attrs["nprocs"] = np.array(nprocs)
-                f.attrs["n_fields"] = np.array([len(field_names)])
+                f.attrs["dims"] = np.array(dset_info.global_shape)
+                f.attrs["nprocs"] = np.array(dset_info.nprocs)
+                f.attrs["n_fields"] = np.array([len(dset_info.field_names)])
                 _add_standard_header_attrs(f)
                 _write_domain_prop_attrs(
-                    f, global_shape=global_shape, domain_info=domain_info
+                    f,
+                    global_shape=dset_info.global_shape,
+                    domain_info=dset_info.domain_info,
                 )
                 # create the datasets that will hold the fields
-                for field_name in field_names:
+                for field_name in dset_info.field_names:
                     f[field_grp].create_dataset(
                         name=field_name, shape=field_dset_shape, dtype="f8"
                     )
 
             # actually record the field data
-            for field_name in field_names:
+            for field_name in dset_info.field_names:
                 f[field_grp][field_name][dst_idx] = global_arrays[field_name][src_slc]
     return global_arrays, fname_template.format(blockid=0)
 
 
-_SIMPLE_DOMAIN_PROPS = DomainInfo(
-    domain_left_edge_kpc=(-25.0, -25.0, -25.0), domain_width_kpc=(100.0, 100.0, 100.0)
-)
+def _get_common_dset_info_objs():
+    kwargs = {
+        "domain_info": DomainInfo(
+            domain_left_edge_kpc=(-25.0, -25.0, -25.0),
+            domain_width_kpc=(100.0, 100.0, 100.0),
+        ),
+        "field_names": ["density", "momentum_x"],
+    }
+
+    return (
+        SyntheticDatasetInfo(nprocs=(1, 1, 1), global_shape=(8, 8, 8), **kwargs),
+        SyntheticDatasetInfo(nprocs=(2, 2, 2), global_shape=(4, 16, 8), **kwargs),
+        SyntheticDatasetInfo(nprocs=(1, 4, 2), global_shape=(4, 16, 8), **kwargs),
+    )
+
+
+_COMMON_DSET_INFO_OBJS = _get_common_dset_info_objs()
 
 
 _CASES = [
     {
-        "nprocs": (1, 1, 1),
-        "global_shape": (8, 8, 8),
+        "dset_info": _COMMON_DSET_INFO_OBJS[0],
         "data_format": ChollaDataFmt.DISTRIBUTED,
-        "domain_info": _SIMPLE_DOMAIN_PROPS,
     },
     {
-        "nprocs": (2, 2, 2),
-        "global_shape": (4, 16, 8),
+        "dset_info": _COMMON_DSET_INFO_OBJS[1],
         "data_format": ChollaDataFmt.DISTRIBUTED,
-        "domain_info": _SIMPLE_DOMAIN_PROPS,
     },
     {
-        "nprocs": (1, 4, 2),
-        "global_shape": (4, 16, 8),
+        "dset_info": _COMMON_DSET_INFO_OBJS[2],
         "data_format": ChollaDataFmt.DISTRIBUTED,
-        "domain_info": _SIMPLE_DOMAIN_PROPS,
     },
     # there no point going through lots of varieties of ChollaDataFmt.LEGACY_CONCAT
     # -> the files always look very similar to each other
     {
-        "nprocs": (2, 2, 2),
-        "global_shape": (4, 16, 8),
+        "dset_info": _COMMON_DSET_INFO_OBJS[0],
         "data_format": ChollaDataFmt.LEGACY_CONCAT,
-        "domain_info": _SIMPLE_DOMAIN_PROPS,
     },
     # it's definitely worth checking ChollaDataFmt.LEGACY_CONCAT when there is only
     # 1 process as well as when there are multiple processes
     {
-        "nprocs": (1, 1, 1),
-        "global_shape": (8, 8, 8),
+        "dset_info": _COMMON_DSET_INFO_OBJS[0],
         "data_format": ChollaDataFmt.CONCAT,
-        "domain_info": _SIMPLE_DOMAIN_PROPS,
     },
     {
-        "nprocs": (2, 2, 2),
-        "global_shape": (4, 16, 8),
+        "dset_info": _COMMON_DSET_INFO_OBJS[1],
         "data_format": ChollaDataFmt.CONCAT,
-        "domain_info": _SIMPLE_DOMAIN_PROPS,
+    },
+    {
+        "dset_info": _COMMON_DSET_INFO_OBJS[2],
+        "data_format": ChollaDataFmt.CONCAT,
     },
 ]
 
@@ -311,9 +346,7 @@ def test_load(tmp_path, kwargs):
     # -> global_arr is a dict that maps each field_name to an array that holding the
     #    fully concatenated array that holds the expected field values
     # -> root_fname is the path that should be passed to yt.load
-    global_arr, root_fname = _generate_files(
-        root_path=str(tmp_path / "0"), field_names=["density", "momentum_x"], **kwargs
-    )
+    global_arr, root_fname = _generate_files(root_path=str(tmp_path / "0"), **kwargs)
 
     # Step 2: load the dataset and build a covering grid spanning the full domain
     # -> accessing a field from the covering grid should return a field with the same
